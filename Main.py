c9256c3ef0256501e8565ed77e3f9f9591ebdab8
hohuy852 <57467992+hohuy852@users.noreply.github.com>
2024-01-03 22:49:42 +0700

    update

--- Main.py
+++ Main.py
@@ -1,10 +1,11 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from MainWindow import MainWindow
-        
+from PyQt5 import QtCore
+from Windows.Login.LoginWindow import LoginWindow
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
-    window = MainWindow()
+    window = LoginWindow()
+    window.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+    window.setAttribute(QtCore.Qt.WA_TranslucentBackground)
     window.show()
     sys.exit(app.exec_())
